d2b17b09b1a17cef57a008c2b4d03d7a60371b12
Fernando Castellanos <fernando.castellanos@hey.com>
2025-03-02 15:24:04 -0800

    Fix migration files

--- db/migrate/20241027081222_rename_accounts_to_organizations.rb
+++ db/migrate/20241027081222_rename_accounts_to_organizations.rb
@@ -1,6 +1,6 @@
-class RenameOrganizationsToOrganizations < ActiveRecord::Migration[8.1]
+class RenameAccountsToOrganizations < ActiveRecord::Migration[8.0]
   def change
-    rename_table :organizations, :organizations
+    rename_table :accounts, :organizations
     rename_table :organization_users, :memberships
     rename_column :memberships, :organization_id, :organization_id
     rename_column :inboxes, :organization_id, :organization_id
